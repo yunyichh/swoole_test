f05538511797af052db6901540e9add48a835ff7
unknown <yunyichh@outlook.com>
2020-07-25 18:15:35 +0800

    1

--- http_server.php
+++ http_server.php
@@ -5,7 +5,7 @@
  * Date: 2020/7/25
  * Time: 17:36
  */
-$http = new Swoole\Http\Server("127.0.0.1", 80);
+$http = new Swoole\Http\Server("132.232.40.19", 80);
 $http->on('request', function ($request, $response) {
     $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
 });
