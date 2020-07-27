393757a12d96d546b50d23e3bbeb282a29b9bb03
unknown <yunyichh@outlook.com>
2020-07-27 15:22:29 +0800

    1

--- co_http_server.php
+++ co_http_server.php
@@ -6,7 +6,7 @@
  * Time: 14:57
  */
 Co\run(function () {
-    $server = new Co\Http\Server("127.0.0.1", 80, false);
+    $server = new Co\Http\Server("132.232.40.19", 80, false);
     $server->handle('/', function ($request, $response) {
         $response->end("<h1>Index</h1>");
     });
